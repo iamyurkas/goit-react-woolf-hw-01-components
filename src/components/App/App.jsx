03016e0ffc9ad355686aab06a1e43e0e1b1ca30b
Iurii <iamyurkas@gmail.com>
2024-04-09 16:27:40 +0100

    Moved App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import user from '../data/user.json';
-import Profile from './Profile/Profile';
+import user from 'data/user.json';
+import Profile from '../Profile/Profile';
 
 
 // import data from '../data/data.json';
